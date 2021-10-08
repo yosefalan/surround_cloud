94524a699684ea636629be458e625707932e15f1
yosefalan <yosefalan@DESKTOP-SU3N0TR.localdomain>
2021-10-08 14:52:47 -0500

    backend test routes commented out

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-
-router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
-});
+// Test routes
+// router.get('/hello/world', function(req, res) {
+//   res.cookie('XSRF-TOKEN', req.csrfToken());
+//   res.send('Hello World!');
+// });
 
 const apiRouter = require('./api');
 

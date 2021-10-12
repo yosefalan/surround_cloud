06505ccff1899cf3f8de2fcf2fbe6b3ac3f30441
yosefalan <yosefalan@DESKTOP-SU3N0TR.localdomain>
2021-10-12 01:11:50 -0500

    continue with landing page styling

--- frontend/src/components/LandingPageCarousel/LandingPageCarousel.js
+++ frontend/src/components/LandingPageCarousel/LandingPageCarousel.js
@@ -4,6 +4,12 @@ import * as sessionActions from "../../store/session";
 import Navigation from '../Navigation';
 import thom from './images/thom.jpg'
 // import trent from './images/trent.jpg'
+import img1 from './images/image1.png'
+import img2 from './images/image2.png'
+import img3 from './images/image3.png'
+import img4 from './images/image4.png'
+import img5 from './images/image5.png'
+import logo from './images/logo.png'
 import styles from './LandingPageCarousel.module.css'
 import LoginFormModal from '../LoginFormModal';
 import SignupFormModal from '../SignupFormModal';
@@ -18,21 +24,28 @@ const LandingPageCarousel = () => {
 
   return (
     <>
-      <div className={styles.carouselContainer}>
-        <div className={styles.nav}>
-          {/* <Navigation isLoaded={isLoaded} /> */}
-          <LoginFormModal className={styles.loginButton} />
-          <SignupFormModal className={styles.signinButton}/>
+      <div className={styles.mainContainer}>
+        <div className={styles.carouselContainer}>
+          <div className={styles.nav}>
+            {/* <Navigation isLoaded={isLoaded} /> */}
+            <img src={logo} className={styles.logo}></img>
+            <LoginFormModal />
+            <SignupFormModal />
+          </div>
+          <div>
+            <img src={thom} className={styles.slide}></img>
+          </div>
+          {/* <div>
+            <img src={trent} className={styles.slide}></img>
+          </div> */}
         </div>
-        <div>
-          <img src={thom} className={styles.slide}></img>
-        </div>
-        {/* <div>
-          <img src={trent} className={styles.slide}></img>
-        </div> */}
-      </div>
-        <div>
-
+          <div className={styles.albumArtContainer}>
+            <img src={img1} className={styles.albumArt}></img>
+            <img src={img2} className={styles.albumArt}></img>
+            <img src={img3} className={styles.albumArt}></img>
+            <img src={img4} className={styles.albumArt}></img>
+            <img src={img5} className={styles.albumArt}></img>
+          </div>
         </div>
     </>
   )
--- frontend/src/components/SignupFormModal/index.js
+++ frontend/src/components/SignupFormModal/index.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import { Modal } from '../../context/Modal';
 import SignupForm from './SignupForm';
+import styles from './SignupForm.module.css'
 
 function SignupFormModal() {
   const [showModal, setShowModal] = useState(false);
 
   return (
     <>
-      <button onClick={() => setShowModal(true)}>Create Account</button>
+      <button onClick={() => setShowModal(true)} className={styles.signinButton}>Create Account</button>
       {showModal && (
         <Modal onClose={() => setShowModal(false)}>
           <SignupForm />

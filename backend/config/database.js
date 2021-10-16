03e2ef92aa97a87c562e58b23877f056bea8c303
yosefalan <yosefalan@DESKTOP-SU3N0TR.localdomain>
2021-10-15 21:19:33 -0500

    first deployment

--- backend/config/database.js
+++ backend/config/database.js
@@ -16,7 +16,7 @@ module.exports = {
     seederStorage: 'sequelize',
   },
   production: {
-    use_env_variable: 'DATABASE_URL',
+    use_env_variable: 'Dpostgres://oziklkedmtgepz:a7c2996c982bc93064392b92a943e2a112a9cc43f4e46f5e52ef421b32b35471@ec2-100-24-169-249.compute-1.amazonaws.com:5432/daa5gvllkmh9q5',
     dialect: 'postgres',
     seederStorage: 'sequelize',
     dialectOptions: {
